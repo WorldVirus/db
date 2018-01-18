b29063dc3dc82d83b2ab94f2967699da16270288
WorldVirus <metallika956@gmail.com>
2018-01-18 22:14:19 +0300

    s2 in wbpck add node_version=product and zip for express, delete pm2

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,22 +4,14 @@ const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge( {
-    entry:[
-        'babel-polyfill',
-        "./scripts/MainClassStarterApp.js"],
+    entry:
+        "./scripts/MainClassStarterApp.js",
     output: {
         filename: "dist/index.js"
     },
 
     target: 'node',
 
-    module: {
-        loaders: [{
-            test: /\.js?$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader'
-        }]
-    },
     devtool: 'source-map',
     plugins: [
         new UglifyJSPlugin({
